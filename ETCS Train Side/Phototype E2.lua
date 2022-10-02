11229e047f7d9449e6f9bb2be0b1eb9bfe08709e
Siravijbb <siravijbb@gmail.com>
2022-10-03 00:36:04 +0700

    Update Phototype E2.lua

--- ETCS Train Side/Phototype E2.lua
+++ ETCS Train Side/Phototype E2.lua
@@ -1,18 +1,24 @@
 @name ETCS train side
-@inputs Recev W S A D Space Deadman SpeedKMH Active
-@outputs Brake Emerbrake Speed LimitSpeed Buzzer BuzzerEmer Sector ReadM
+@inputs Nowspeed Nextspeed W S A D Space Deadman SpeedKMH Active
+@outputs Brake Emerbrake Speed LimitSpeed Buzzer BuzzerEmer Sector ReadM Nextlimit EmerbrakeCounter
 Deadtime = 5000
 Sector = 1
 ReadM = 1
 Buzzer = 0
-
-
-LimitSpeed = Recev
+Recev = Nowspeed
 Speed = SpeedKMH
-if(Active <= 1){
-    timer("Deadman",Deadtime)
-}
-if(Space <= 1 || Deadman <= 1){
+LimitSpeed = Recev
+Nextlimit = Nextspeed
+
+if(Active != 1){
+    Emerapply = 100
+    Brake = 100
+    stopAllTimers()
+}else{
+    timer("Deadman",Deadtime)}
+
+
+if(Space >= 1 || Deadman >= 1){
     stoptimer("Deadman")
     stoptimer("Emerapply")
     timer("Deadman",Deadtime)
@@ -25,6 +31,12 @@ if(clk("Deadman")){
 if(clk("Emerapply")){
     Emerbrake = 100
     Buzzer = 100
+    EmerbrakeCounter = 1
+}
+if(EmerbrakeCounter == 1){
+    Emerbrake = 100
+    Buzzer = 100
+    EmerbrakeCounter = 1
 }
 if(LimitSpeed <= SpeedKMH && LimitSpeed+10 >= SpeedKMH){
     Brake = 100
@@ -34,9 +46,29 @@ elseif(LimitSpeed <= SpeedKMH && LimitSpeed+10 <= SpeedKMH){
     Emerbrake = 100
     Brake = 100
     BuzzerEmer = 100
+    EmerbrakeCounter = 1
 }
-if(LimitSpeed >= SpeedKMH && LimitSpeed+10 >= SpeedKMH){
+if(LimitSpeed >= SpeedKMH && LimitSpeed+10 >= SpeedKMH && EmerbrakeCounter == 0 && Active == 1){
     Emerbrake = 0
     Brake = 0
     BuzzerEmer = 0
+}
+if(EmerbrakeCounter == 1 && SpeedKMH <= 1 && Active == 1 || Space >= 1){
+    EmerbrakeCounter = 0
+    Emerbrake = 0
+    Brake = 0
+    BuzzerEmer = 0
+}
+if(LimitSpeed == 0 && SpeedKMH >= 0){
+    Emerbrake = 100
+    Brake = 0
+    BuzzerEmer = 100
+}
+if(LimitSpeed == 0 && SpeedKMH <= 3){
+    Emerbrake = 100
+    Brake = 0
+    BuzzerEmer = 0
+}
+if(SpeedKMH <= 2){ 
+    Brake = 100
 }
