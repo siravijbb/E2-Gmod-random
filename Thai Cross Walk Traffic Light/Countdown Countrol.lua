3794911afe5f94ae30e9963b6a4bf06f654a2aa9
Siravijbb <siravijbb@gmail.com>
2022-11-27 20:01:58 +0700

    mega f

--- Thai Cross Walk Traffic Light/Countdown Countrol.lua
+++ Thai Cross Walk Traffic Light/Countdown Countrol.lua
@@ -1,34 +1,49 @@
 @name Count down
-@inputs CrossTimmers Roadtimer Timmerstart Active CrossLimit RoadLimit
-@outputs CrossCountdowntime RoadCountDown State ResetRoad ResetCross
+@inputs CrossTimmers Roadtimer Timmerstart 
+@outputs CrossCountdowntime RoadCountDown State ResetRoad ResetCross Active Debut CrossLimit RoadLimit
 
 CrossCountdowntime = CrossTimmers - CrossLimit
 RoadCountDown = Roadtimer - RoadLimit
 
-State = 1
-
-if(Timmerstart == 1){
+if(Timmerstart == 1 ){
 Active = 1
 CrossLimit = 13
 RoadLimit = 10
+ResetCross = 0
+ResetRoad = 0
+
+if(State == 0 || State == 4){
+State = 1
+Debut = 1}
 }
-if(RoadCountDown <= 0 && State = 1){
-    
-    ResetRoad = 1
-    ResetRoad = 0
+
+else{
+    CrossLimit = 0
+RoadLimit = 0
+State = 0
+}
+if(State == 4){
+    CrossLimit = 0
+    RoadLimit = 0
+    State = 0
+}
+if(Roadtimer >= 1 && State > 0)
+if((Roadtimer >= 9.8 && State == 1) || Debut == 3){
+    Debut = 3
     RoadLimit = 3
     State = 2
 }
-if(CrossCountdowntime <= 0 && State = 2){
-    
-    ResetCross = 1
-    RedCross = 0
+if((CrossTimmers >= 12.8 && State == 2) || Debut == 4 ){
+    Debut = 4
+    CrossLimit = 1
     CrossLimit = 10
+    RoadLimit = 1
+    RoadLimit = 13
     State = 3
 }
-if(RoadCountDown <= 0 && State = 3){
-    ResetRoad = 1
+if((Roadtimer >= 12.8 && State == 3) || Debut == 5 ){
+    Debut = 5
     Active = 0
-    RoadLimit = 3
-    State = 1
+    State = 4
+    
 }
