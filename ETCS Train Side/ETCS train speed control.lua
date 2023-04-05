b84e5717ede80b381e1a55fab6af2fa0ffe38128
Siravijbb <siravijbb@gmail.com>
2023-04-05 23:13:16 +0700

    Refactor:Unused Logic

--- ETCS Train Side/ETCS train speed control.lua
+++ ETCS Train Side/ETCS train speed control.lua
@@ -7,36 +7,11 @@ Nextlimit =1
 LimitSpeed =1
 Buzzer = 0
 
-if($Nowspeedin > Nextspeed){
+if(Nowspeedin > Nextspeed){
     Nowspeed = Nowspeedin
-    Nextspeed = Nowspeedin
-    print("2A")}
-if($Nextspeedin > Nowspeed){
+    Nextspeed = Nextspeedin
+    print("2A REDUCE SPEED")}
+if(Nextspeedin > Nowspeed){
     Nowspeed = Nextspeedin
     Nextspeed = Nextspeedin
-    print("2B")}
-########################################
-if($Nowspeedin  < Nowspeed){
-stopAllTimers()
-Nextspeedin = Nowspeedin
-timer("Nowspeedinnew",10000)
-Buzzer = 100
-print("1A")
-}
-if(clk("Nowspeedinnew")){
-Nowspeed = Nowspeedin
-Nextspeed = Nowspeedin
-Buzzer = 0
-print("A")}
-if($Nextspeedin  < Nowspeed){
-stopAllTimers()
-Nowspeedin = Nextspeedin
-timer("Nextspeedinnew",10000)
-Buzzer = 100
-print("1B")
-}
-if(clk("Nextspeedinnew")){
-Nowspeed = Nowspeedin
-Nextspeed = Nowspeedin
-Buzzer = 0
-print("B")}
+    print("2B SPEED CHANGED")}
