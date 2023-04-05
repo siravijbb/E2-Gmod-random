ce933800a9665e5252d668e59c0815d4ad05e604
Siravijbb <siravijbb@gmail.com>
2023-04-06 02:39:51 +0700

    new logic for OCC

--- EYCS OCC  Side/ETCS Section Speed Controller.lua
+++ EYCS OCC  Side/ETCS Section Speed Controller.lua
@@ -1,1 +1,18 @@
+@name ETCS train side
+@inputs  Nowspeedin Nextspeedin sectionSelect inNextOveride nextspeedNextSign
+@outputs Nowspeed Nextspeed ETCSOutspeed
 
+ETCSOutspeed = Nowspeed
+sectionNumber = 1 #Change as section
+nextOveride = inNextOveride
+Nextspeed = nextspeedNextSign
+
+if(sectionSelect == sectionNumber){
+    Nowspeed = Nowspeedin
+    if(nextOveride == 1){ 
+        Nextspeed = Nextspeedin ##overide next sign speed
+    }
+    else{
+        Nextspeed = nextspeedNextSign
+    }
+}
