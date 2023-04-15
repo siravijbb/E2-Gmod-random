7bcfe0ab39fd468b22c5d7ed027c7b6c3b2bd8e4
Siravij Praevisavakij <siravijbb@gmail.com>
2023-04-15 07:16:45 +0700

    added:Light control and blink for future crossing

--- EYCS OCC  Side/ETCS Section Speed Controller.lua
+++ EYCS OCC  Side/ETCS Section Speed Controller.lua
@@ -1,5 +1,5 @@
 @name ETCS Track Side
-@inputs  Nowspeedin Nextspeedin SectionSelect InNextOveride NextspeedNextSign NextsecondspeedNextSign AxelCounter
+@inputs  Nowspeedin Nextspeedin SectionSelect InNextOveride NextspeedNextSign NextsecondspeedNextSign AxelCounter CrossingGate
 @outputs Nowspeed Nextspeed ETCSOutspeed
 
 ETCSOutspeed = Nowspeedin 
@@ -25,4 +25,55 @@ else{
         Nextspeed = NextspeedNextSign
     } 
 
+############################### Light
+
+if(20 >= ETCSOutspeed && ETCSOutspeed != 0 ){
+    Red = 255
+    Yellow = 255
+    Green = 0
+
+}
+elseif(40 > ETCSOutspeed && ETCSOutspeed >=    20){
+    Red = 0
+    Yellow = 255
+    Green = 0
+
+}
+elseif(60 > ETCSOutspeed && ETCSOutspeed >= 40){
+    Red = 0
+    Yellow = 255
+    Green = 255
+
+}
+elseif(60 <= ETCSOutspeed ){
+    Red = 0
+    Yellow = 0
+    Green = 255
+
+}
+else{    
+    Red = 255
+    Yellow = 0
+    Green = 0
+  }
+
+if(CrossingGate == 1){
+    timer("Blink",2000)
+    if(clk("Blink")){
+        stoptimer("Blink")
+        CrossBlink = 1
+        timer("BlinkClose",2000)
+    }
+    if(clk("BlinkClose")){
+        stoptimer("BlinkClose")
+        CrossBlink = 0
+    }
+}
+
+if(CrossBlink == 1){
+    Blink = 255
+    }
+else{
+    Blink = 0
+    }
 
