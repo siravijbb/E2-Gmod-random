1897badb33f40108d0b28add85df50ddde7cf748
Siravij Praevisavakij <siravijbb@gmail.com>
2023-04-15 07:03:41 +0700

    dl;fks;dl

--- EYCS OCC  Side/ETCS Section Speed Controller.lua
+++ EYCS OCC  Side/ETCS Section Speed Controller.lua
@@ -1,25 +1,28 @@
 @name ETCS Track Side
-@inputs  Nowspeedin Nextspeedin SectionSelect InNextOveride NextspeedNextSign
+@inputs  Nowspeedin Nextspeedin SectionSelect InNextOveride NextspeedNextSign NextsecondspeedNextSign AxelCounter
 @outputs Nowspeed Nextspeed ETCSOutspeed
 
-ETCSOutspeed = Nowspeedin
+ETCSOutspeed = Nowspeedin 
 SectionNumber = 1 #Change as section
-NextOveride = InNextOveride
-Nextspeed = NextspeedNextSign
+
 
 if(SectionSelect == SectionNumber){
+    NextOveride = InNextOveride
     Nowspeed = Nowspeedin
     OldNowspeed = Nowspeedin
-    if(NextOveride == 1){ 
+    if(NextOveride == 1){  ##if next input sign is overide
         Nextspeed = Nextspeedin ##overide next sign speed
     }}
 else{
-        if(NextspeedNextSign == 0 && NextOveride == 0){
+    if($AxelCounter){
+    Nowspeed = 0
+    }
+    elseif(NextsecondspeedNextSign == 0 && NextOveride == 0){ ##if next second sign is 0
         OldNowspeed = Nowspeed
         Nowspeed = 30}
-        else{
+        else{ ##if next  sign is not 0
         Nowspeed = OldNowspeed}  
         Nextspeed = NextspeedNextSign
-    }
+    } 
 
 
